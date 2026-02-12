53482be2ffbb2795ce305176d8d66c9261dc97b9
Abhishakes <abhishakes@coders-desk.com>
2026-02-12 12:07:16 +0530

    Add staging config to database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -25,5 +25,13 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false
+    },
+    staging: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false
     }
 };
